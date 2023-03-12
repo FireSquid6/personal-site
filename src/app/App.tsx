99ea29dd996e6be712f2ddc95c1e4af34b5e85c4
Jonathan Deiss <39541470+FireSquid6@users.noreply.github.com>
2023-03-12 16:34:16 -0500

    update thing

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import Header from '../components/header/Header';
 import reactLogo from './assets/react.svg';
+import About from '../components/about/About';
 
 function App() {
   const [count, setCount] = useState(0)
@@ -8,6 +9,7 @@ function App() {
   return (
     <>
       <Header />
+      <About title="Jonathan Deiss" subtitle="Fullstack, Software, and Game Developer" />
     </> 
   )
 }
--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,12 +1,25 @@
 import './Header.css';
+import { useState } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBars } from '@fortawesome/free-solid-svg-icons';
 
 export default function Header() {
+  const [headerActive, setHeaderActive] = useState(false)
+  const handleClick = () => {
+    setHeaderActive(!headerActive);
+  }
+
   return (
     <header>
-      <a data-testid="aboutLink" href="#about">About</a>
-      <a data-testid="projectsLink" href="#projects">Projects</a>
-      <a data-testid="experienceLink" href="#experience">Experience</a>
-      <a data-testid="linksLink" href="#links">Links</a>
+      <button className={`menuButton ${headerActive ? "tilt" : ""}`} onClick={handleClick}>
+        <FontAwesomeIcon icon={faBars} size="2x"></FontAwesomeIcon>
+      </button>
+      <div className={`headerLinks ${headerActive ? "active" : "inactive"}`}>
+        <a data-testid="aboutLink" href="#about">About</a>
+        <a data-testid="projectsLink" href="#projects">Projects</a>
+        <a data-testid="experienceLink" href="#experience">Experience</a>
+        <a data-testid="linksLink" href="#links">Links</a>
+      </div>
     </header>
   );
 }
