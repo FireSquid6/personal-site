cdd840dcecf4573f1fa236d7497d923a0c7e4252
firesquid06 <jdeiss06@gmail.com>
2023-07-02 17:40:45 -0500

    add header styling

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -3,9 +3,11 @@ import { NavItem } from "@/components/NavItem/NavItem";
 
 export default function Header() {
   return (
-    <header>
-      <h1>firesquid</h1>
-      <nav>
+    <header className="flex flex-row bg-bg_dark align-center py-3 sticky top-0">
+      <div className="flex flex-col align-center justify-center w-full m-2">
+        <h1 className="text-tealtext-center sm:text-5xl">firesquid</h1>
+      </div>
+      <nav className="flex flex-row align-center justify-around">
         {navItems.map((item, i) => (
           <NavItem key={i} label={item.label} href={item.href} />
         ))}
--- components/NavItem/NavItem.tsx
+++ components/NavItem/NavItem.tsx
@@ -9,10 +9,15 @@ export interface NavItemProps {
 export function NavItem({ label, href }: NavItemProps) {
   const pathname = usePathname();
   return (
-    <div>
-      <a href={href} className={`${pathname === href ? "underline" : ""}`}>
-        {label}
-      </a>
-    </div>
+    <a
+      href={href}
+      className={`${
+        pathname === href
+          ? "underline text-orange hover:text-orange"
+          : "text-blue hover:text-blue"
+      } p-4 mx-2 bg-black1 rounded-xl hover:bg-dark5 transition-all`}
+    >
+      {label}
+    </a>
   );
 }
