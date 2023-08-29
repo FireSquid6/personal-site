55b6f4faee89ec5a76c2986c3e6fb694c87528e3
firesquid <jdeiss06@gmail.com>
2023-08-28 21:59:38 -0500

    add skills hover feature

--- components/home/skills.tsx
+++ components/home/skills.tsx
@@ -14,10 +14,24 @@ export default function Skills() {
 
   return (
     <section className="flex flex-col items-center justify-center">
-      <div className="w-full p-2 m-4 border border-green flex text-green">
-        <h2 className="text-2xl">
-          {hoveredSkill.name} - {hoveredSkill.level}
+      <div className="w-full p-4 m-4 border flex-col border-green flex text-fg h-32 rounded-lg bg-bg_dark">
+        <h2 className="text-2xl my-2">
+          <span className="text-yellow">{hoveredSkill.name}</span> -{" "}
+          <span className="text-green">{hoveredSkill.level}</span>
         </h2>
+        {hoveredSkill.info && <p>{hoveredSkill.info}</p>}
+        <span className="h-full"></span>
+
+        <ul className="flex flex-wrap">
+          {hoveredSkill.projects.map((project) => (
+            <div
+              key={project}
+              className="mr-2 bg-dark border-r border-l border-fg px-1"
+            >
+              {` ${project} `}
+            </div>
+          ))}
+        </ul>
       </div>
 
       <div className="flex flex-wrap justify-center">
