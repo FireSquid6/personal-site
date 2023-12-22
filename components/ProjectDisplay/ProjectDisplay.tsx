da45c4b5d00c6cc8706c3b1edc9b74b765e90db9
firesquid6 <jdeiss06@gmail.com>
2023-12-22 17:48:52 -0600

    add h1 to project display

--- components/ProjectDisplay/ProjectDisplay.tsx
+++ components/ProjectDisplay/ProjectDisplay.tsx
@@ -4,20 +4,23 @@ import Project from "../Project/Project";
 export default function ProjectDisplay() {
   let i = 0;
   return (
-    <div className="my-16 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 px-4">
-      {projects.map((project) => {
-        i += 1;
-        return (
-          <Project
-            key={i}
-            title={project.title}
-            description={project.description}
-            imageUrl={project.imageUrl}
-            url={project.url}
-            skills={project.skills}
-          />
-        );
-      })}
+    <div className="my-16">
+      <h1 className="text-5xl text-center my-4">I've built:</h1>
+      <div className=" grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 px-4">
+        {projects.map((project) => {
+          i += 1;
+          return (
+            <Project
+              key={i}
+              title={project.title}
+              description={project.description}
+              imageUrl={project.imageUrl}
+              url={project.url}
+              skills={project.skills}
+            />
+          );
+        })}
+      </div>
     </div>
   );
 }
