a895f86191eb89088b6f5be25fdca23bb55eec4f
firesquid6 <jdeiss06@gmail.com>
2023-12-28 13:11:15 -0600

    add all of my projects

--- components/ProjectDisplay/projects.ts
+++ components/ProjectDisplay/projects.ts
@@ -8,6 +8,30 @@ export interface Project {
 
 export const projects: Project[] = [
   {
+    title: "Inkdocs",
+    description:
+      "An incredibly fast documentation site builder that can be used as a replacement for docusaurus and starlight. Rejects the idea that you need React to make a component based frontend. Currently in development",
+    skills: ["Typescript", "Bun", "HTML", "CSS"],
+    url: "https://github.com/firesquid6/inkdocs",
+    imageUrl: "/images/inkdocs.png",
+  },
+  {
+    title: "Negtd",
+    description:
+      "A text based todo list app. It uses a modified system of GTD to help you get things done.",
+    skills: ["Linux", "Go"],
+    url: "https://github.com/firesquid6/negtd",
+    imageUrl: "/images/negtd.png",
+  },
+  {
+    title: "Powertool",
+    description:
+      "A command line tool for quickly creating projects. Has a built in rofi meny for selecting templates. Currently in development",
+    skills: ["Linux", "Go"],
+    url: "https://github.com/firesquid6/powertool",
+    imageUrl: "/images/powertool.png",
+  },
+  {
     title: "This Website",
     description:
       "I've rebuilt this website three times. It was originally a Hugo site, then I remade it as an SPA with react and vite, and now I'm remaking it again with NextJS and tailwind",
@@ -37,5 +61,36 @@ export const projects: Project[] = [
     skills: ["node.js", "javascript"],
     url: "https://github.com/firesquid6/gdvm",
     imageUrl: "/images/gdvm.png",
+  },
+  {
+    title: "Mathworld",
+    description:
+      "A game I worked on at my internship while in high school. It's a math based puzzle game designed to teach elementary schoolers the basics of arithimatic",
+    skills: ["Godot", "GDScript", "Git"],
+    url: "https://github.com/sindrsh/mathworld",
+    imageUrl: "/images/mathworld.png",
+  },
+  {
+    title: "Dodge Roll",
+    description: "A top down shooter built in Godot for the GMTK game jam",
+    skills: ["Godot", "GDScript"],
+    url: "https://github.com/firesquid6/dodge-roll",
+    imageUrl: "/images/dodge-roll.png",
+  },
+  {
+    title: "Poltergeist Heist",
+    description:
+      "A game made for the Godot Wild Jam. It's a 2d puzzle platformer about controlling multiple ghosts.",
+    skills: ["Godot", "GDScript"],
+    url: "https://github.com/firesquid6/poltergeist-heist",
+    imageUrl: "/images/poltergeist-heist.png",
+  },
+  {
+    title: "Teleorb",
+    description:
+      "My first game! Created in Gamemaker Studio 2 while I was in 9th grade.",
+    skills: ["Gamemaker Studio 2", "GML"],
+    url: "https://github.com/firesquid6/teleorb-classic",
+    imageUrl: "/images/teleorb.png",
   },
 ];
