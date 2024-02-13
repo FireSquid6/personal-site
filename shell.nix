0dafef07eee45bc5ead999d68c604149af4ecf57
firesquid6 <jdeiss06@gmail.com>
2024-02-13 14:11:12 -0600

    add a better nix shell

--- shell.nix
+++ shell.nix
@@ -1,12 +1,20 @@
+# Help! I'm a nix user and it isn't working!
+#
+# try running these commands:
+# sudo nix-channel --add https://nixos.org/channels/nixos-unstable nixos-unstable
+# sudo nix-channel --update
+# then run nix-shell again
+
 let
-  unstable = import (fetchTarball https://nixos.org/channels/nixos-unstable/nixexprs.tar.xz) { };
+  unstable = import <nixos-unstable> { config = { allowUnfree = true; }; };
 in
 { nixpkgs ? import <nixpkgs> { } }:
 with nixpkgs; mkShell {
+  DOCKER_BUILDKIT = "1";
   buildInputs = [
     unstable.bun
     nodejs_20
-    netlify-cli
+    flyctl
     libgcc
   ];
 }
