509dc85cfab286a4482957a93b41e217b51c24fd
firesquid6 <jdeiss06@gmail.com>
2024-02-13 21:16:22 -0600

    add guide on next vitest

--- components/root.tsx
+++ components/root.tsx
@@ -7,9 +7,9 @@ export default function Root({
   children: JSX.Element;
 }): JSX.Element {
   return (
-    <body class="flex min-h-[100vh] flex-col">
+    <body class="flex min-h-[100vh] max-w-[80rem] mx-auto flex-col">
       <Header />
-      <main id="layout" class="flex flex-col flex-1 h-full mx-8">
+      <main id="layout" class="flex flex-col  mx-8 flex-1 h-full">
         {children}
       </main>
       <Footer />
--- layouts/writing-hub.tsx
+++ layouts/writing-hub.tsx
@@ -9,17 +9,40 @@ const WritingHub: Layout = (children, _, artifacts) => {
   return (
     <Root>
       <>
-        <h1>Stuff I've Written</h1>
+        <h1 class="text-center text-red1 mb-16">Stuff I've Written</h1>
 
-        <div>
-          {writingPages.map((page) => (
-            <SwapLink target="layout" href={page.href}>
-              <>
-                <h2>{page.title}</h2>
-                <p class="italic">{page.description}</p>
-              </>
-            </SwapLink>
-          ))}
+        <div class="flex flex-col">
+          {writingPages.map((page) => {
+            const date = new Date(page.date);
+            const dateString = date.toLocaleDateString("en-US", {
+              year: "numeric",
+              month: "long",
+              day: "numeric",
+            });
+
+            let tagString = "";
+            for (let i = 0; i < page.tags.length; i++) {
+              tagString += page.tags[i];
+              if (i < page.tags.length - 1) {
+                tagString += ", ";
+              }
+            }
+
+            return (
+              <SwapLink
+                target="layout"
+                className="mb-12 hover:translate-x-5"
+                href={page.href}
+              >
+                <>
+                  <h2 class="text-green1">{page.title}</h2>
+                  <p class="text-fg"> {dateString}</p>
+                  <p class="text-fg mb-2"> {tagString}</p>
+                  <p class="italic text-fg_dark">{page.description}</p>
+                </>
+              </SwapLink>
+            );
+          })}
         </div>
 
         <div class="content" id="content">
